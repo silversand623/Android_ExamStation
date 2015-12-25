d1823d60353d7fa3738635ea3c723c557b7d95a2
zhangyingforever <zhangwangwang@126.com>
2015-12-25 14:44:15 +0800

    xiugai setting

--- ExamStationShow/app/src/main/java/com/example/tellyes/examstationshow/SettingActivity.java
+++ ExamStationShow/app/src/main/java/com/example/tellyes/examstationshow/SettingActivity.java
@@ -1,5 +1,6 @@
 package com.example.tellyes.examstationshow;
 
+import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
